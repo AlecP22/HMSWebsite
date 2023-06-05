af724c2ea408c747b8aa23d0449f41e07c73b1b9
AlecP22 <alecpemberton80@gmail.com>
2023-06-05 14:44:10 -0400

    TEST

---
+++ Website (IN PROGRESS)/Script.js
@@ -0,0 +1,41 @@
+const menu = document.querySelector(".Menu2");
+const menuItems = document.querySelectorAll(".Menu-Item");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu2")) {
+    menu.classList.remove("showMenu2");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu2");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+    }
+  )
+
+  let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 3500); 
+}
